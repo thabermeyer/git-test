ef4c49c517147aea13078d6a2e784df62e52e858
Trevor <thabermeyer@gmail.com>
2017-11-07 16:54:23 -0700

    Completed header section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person.js'); the old way before ES6
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes.")
-  }
-}
-
-alert("ABC 123");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
